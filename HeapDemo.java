0d607f5032cc671cd79448188a76c88f45fb6786
ramakrishna <saiduramakrishna25@gmail.com>
2021-03-04 07:26:36 +0530

    Add files via upload

---
+++ HeapDemo.java
@@ -0,0 +1,122 @@
+/*
+	This class is an implementation of MIN Heap
+*/
+
+
+public class HeapDemo
+{
+	final int MAX = 10;
+	private int[] heap= new int[MAX];
+	private int index=-1,temp;
+	
+	public static void main(String args[]){
+		HeapDemo hp=new HeapDemo();
+		int[] ar = new int[]{11,10,3,4,12};	
+		
+		for(int i=0;i<ar.length;i++)
+			hp.insert(ar[i]);
+		
+		hp.heapSort();
+		
+		hp.display();
+	}
+	private void heapSort(){
+		int temp = index;
+		
+		while(index>=0)
+		{
+			int min = heap[0];
+			heap[0] = heap[index];
+			heap[index] = min;
+			index--;
+			
+			heapifyDown(0);
+			display();
+		}
+		
+		index = temp;
+	}
+	
+	private void display(){
+		for(int i=0;i<=index;i++)
+			System.out.print(heap[i]+" ");
+		System.out.println();
+	}
+	
+	public int getMin(){
+		if(index == -1)
+			return -1;
+		
+		return heap[0];
+	}
+	
+	public int deleteMin(){
+		if(index == -1)
+			return -1;
+	
+		int min = heap[0];
+		heap[0] = heap[index--];		
+		heapifyDown(0);
+		return min;		
+	}
+	
+	public boolean insert(int ele)
+	{
+		if((index+1) == MAX)
+			return false;
+		
+		index++;
+		heap[index] = ele;
+		heapifyUp(index);
+		
+		return true;
+	}
+	
+	private void heapifyDown(int i){
+		int l = getLeftChild(i);
+		int r = getRightChild(i);
+		int small = i;
+		
+		if(l< index && heap[small] > heap[l])
+			small = l;
+		if(r<index && heap[small] > heap[r])
+			small = r;
+		
+		if(small != i){
+			//swap both and call heapifyDown
+			int temp = heap[i];
+			heap[i] = heap[small];
+			heap[small] = temp;
+			
+			heapifyDown(small);	
+		}
+	}	
+	
+	private void heapifyUp(int index){
+		int parent = getParent(index);
+		//System.out.println(parent);
+		
+		//comparing child and parent
+		if(parent>=0 && heap[index]<heap[parent])
+		{
+			temp = 	heap[index];
+			heap[index] = heap[parent];
+			heap[parent] = temp;
+			
+			heapifyUp(parent);
+		}
+	}	
+	
+	private int getParent(int i){
+		return (i-1)/2;
+	}
+	
+	private int getLeftChild(int i){
+		return 2*i+1;
+	}
+	
+	private int getRightChild(int i){
+		return 2*i+2;
+	}	
+}
+
